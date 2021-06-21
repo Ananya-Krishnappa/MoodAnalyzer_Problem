19c6319d7a9bbf690ae74e56e00ee96c57b0340c
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-21 22:02:19 +0530

    Added:handling exception

--- src/main/java/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
+++ src/main/java/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
@@ -22,13 +22,20 @@ public class MoodAnalyzer {
 	 * This method compares the message and returns if present or not
 	 * 
 	 * @return Happy or sad
+	 * @throws Exception
 	 */
-	public Mood analyseMood() {
-		if (message.toLowerCase().indexOf(Mood.HAPPY.name().toLowerCase()) != -1) {
+	public Mood analyseMood() throws Exception {
+		try {
+			if (message.toLowerCase().indexOf(Mood.HAPPY.name().toLowerCase()) != -1) {
+				return Mood.HAPPY;
+			} else if (message.toLowerCase().indexOf(Mood.SAD.name().toLowerCase()) != -1) {
+				return Mood.SAD;
+			}
 			return Mood.HAPPY;
-		} else if (message.toLowerCase().indexOf(Mood.SAD.name().toLowerCase()) != -1) {
-			return Mood.SAD;
+		} catch (NullPointerException e) {
+			return Mood.HAPPY;
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
 		}
-		return Mood.HAPPY;
 	}
 }
--- src/test/java/bridgelabz/MoodAnalyzer/MoodAnalyzerTest.java
+++ src/test/java/bridgelabz/MoodAnalyzer/MoodAnalyzerTest.java
@@ -8,22 +8,30 @@ import org.junit.Test;
 public class MoodAnalyzerTest {
 	MoodAnalyzer moodAnalyzer;
 	MoodAnalyzer moodAnalyzer1;
+	MoodAnalyzer moodAnalyzer2;
 
 	@Before
 	public void init() {
 		moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
 		moodAnalyzer1 = new MoodAnalyzer("I am in Happy Mood");
+		moodAnalyzer2 = new MoodAnalyzer(null);
 	}
 
 	@Test
-	public void shouldReturnSadWhenStringContainsSad() {
+	public void shouldReturnSadWhenStringContainsSad() throws Exception {
 		Mood result = moodAnalyzer.analyseMood();
 		assertEquals(Mood.SAD, result);
 	}
 
 	@Test
-	public void shouldReturnHappyWhenStringNotContainsSad() {
+	public void shouldReturnHappyWhenStringNotContainsSad() throws Exception {
 		Mood result = moodAnalyzer1.analyseMood();
+		assertEquals(Mood.HAPPY, result);
+	}
+
+	@Test
+	public void shouldReturnHappyWhenStringIsNull() throws Exception {
+		Mood result = moodAnalyzer2.analyseMood();
 		assertEquals(Mood.HAPPY, result);
 	}
 }
